7e896f93f2378091354556c6e3f35838b4f0fe4f
dustinpfister <dustin.pfister@gmail.com>
2018-06-26 07:56:44 -0400

    serving up csv as json

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 let express = require('express'),
+csvFilePath = './csv/2017_day.csv',
+csv = require('csvtojson'),
 
 app = express();
 
 app.set('port', 8080);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 
-    res.send('okay');
+    csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+
+        res.json(jsonObj);
+
+    });
 
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), ()=>{
 
     console.log('express_visual_analytics is up on port: ' + app.get('port'));
 
