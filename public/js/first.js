995e5bbc141bd11560a043a3414da79718107a0e
dustinpfister <dustin.pfister@gmail.com>
2018-06-27 08:05:23 -0400

    pretty colors

--- public/js/first.js
+++ public/js/first.js
@@ -7,10 +7,11 @@ $.ajax({
 }).done(function (days) {
 
     // a scene is needed to place objects in
-    var scene = new THREE.Scene(),
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xafafaf);
 
     // I will need an camera to look at objects in the scene
-    camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000),
+    var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000),
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -36,13 +37,16 @@ $.ajax({
             //if (jsDate.getMonth() === 0) {
 
             var y = day.users * .125,
+            colors = [0xff8800, 0xffff00, 0x0000ff, 0xff00ff, 0xff0000, 0x00ff00, 0x00ffff],
             c = (i / days.length);
 
             var cube = new THREE.Mesh(new THREE.BoxGeometry(1, y, 1),
                     new THREE.MeshStandardMaterial({
 
-                        color: new THREE.Color(0, day.users / 1000, 0),
-                        emissive: 0x2a2a2a
+                        //color: new THREE.Color(0, day.users / 1000, 0),
+                        color: colors[jsDate.getDay()],
+                        //color: new THREE.Color(0, jsDate.getDay() / 6, 0),
+                        emissive: 0x000000
 
                     }));
 
@@ -66,9 +70,11 @@ $.ajax({
     camera.lookAt(0, 43, 25);
 
     var light = new THREE.PointLight();
-    light.position.set(0,50,0);
-	
-	light.add(new THREE.Mesh(new THREE.SphereGeometry(1,20,20), new THREE.MeshBasicMaterial({color: 0xffffff})))
+    light.position.set(0, 50, 0);
+
+    light.add(new THREE.Mesh(new THREE.SphereGeometry(1, 20, 20), new THREE.MeshBasicMaterial({
+                color: 0xffffff
+            })))
 
     scene.add(light);
 
