11c71093aab6847701cf63761ed74086064f6c15
dustinpfister <dustin.pfister@gmail.com>
2018-06-26 20:39:16 -0400

    setting javaScript time stamps

--- mw/reports.js
+++ mw/reports.js
@@ -1,9 +1,32 @@
 
 module.exports = [
 
+    // get db, and set time stamps
+    function (req, res, next) {
+
+        req.db = require('../public/json/days.json');
+
+        req.db.days.forEach(function (day) {
+
+            if (day.date.match(/\d+\/\d+\/\d+/)) {
+
+                var d = day.date.split('/'),
+
+                jsDate = new Date('20' + d[2], d[0] - 1, d[1]);
+
+                day.timeStamp = jsDate;
+
+            }
+
+        });
+
+        next();
+
+    },
+
     function (req, res) {
 
-        res.send(require('../public/json/2018.json'));
+        res.json(req.db);
 
     }
 
