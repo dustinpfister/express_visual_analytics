97d13ae9cf9a509a703f91585bfd5e3f4b2224c3
dustinpfister <dustin.pfister@gmail.com>
2018-06-29 13:37:28 -0400

    started work on first real work

--- public/js/visual_analytics/works/first/work.js
+++ public/js/visual_analytics/works/first/work.js
@@ -92,7 +92,7 @@ $.ajax({
     var camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000);
 
     var light = new THREE.PointLight();
-    light.position.set(0, 25, 0);
+    //light.position.set(0, 25, 0);
     camera.add(light);
     scene.add(camera);
 
@@ -135,110 +135,6 @@ $.ajax({
     camera.lookAt(80, 0, 0);
     controls.target.set(80, 0, 0);
 
-    // materials API
-    var materials = {
-
-        //  fund out what material to use here
-        set: function (options) {
-
-            options = options || {};
-            options.day = options.day || {
-                day: {}
-            };
-            options.data = options.data || {};
-
-            options.daysBack = options.daysBack || options.data.days.length;
-
-            // we should have a latest day
-            if (options.data.latest) {
-
-                if (options.day.jsDate === options.data.latest.date) {
-
-                    return this['latest'](options);
-
-                }
-
-            }
-
-            // we should have users
-            if (options.day.users) {
-
-                if (Number(options.day.users) === Number(options.data.bestDay)) {
-
-                    return this['bestday'](options);
-
-                }
-
-            }
-
-            return this['time'](options);
-
-        },
-
-        // material for the best day
-        bestday: function (options) {
-
-            return new THREE.MeshStandardMaterial({
-
-                color: new THREE.Color(1, 0, 0),
-                emissive: new THREE.Color(.25, 0, 0)
-
-            });
-        },
-
-        // material for the best day
-        latest: function (options) {
-
-            return new THREE.MeshStandardMaterial({
-
-                color: new THREE.Color(0, 1, 1),
-                emissive: new THREE.Color(0, .25, .25)
-
-            });
-        },
-
-        // standard material for days that are not special
-        time: function (options) {
-
-            var latest = options.data.latest.date,
-            time = latest - options.day.jsDate,
-            per = 1 - (time / (1000 * 60 * 60 * 24 * options.daysBack));
-
-            if (per < 0) {
-                per = 0;
-            }
-
-            if (per > 1) {
-                per = 1;
-            }
-
-            return new THREE.MeshStandardMaterial({
-
-                color: new THREE.Color(per, per, 0),
-                //wireframe:true,
-                transparent: true,
-                opacity: per * .8
-
-            });
-
-        },
-
-        // standard material for days that are not special
-        standard: function (options) {
-
-            return new THREE.MeshStandardMaterial({
-
-                color: new THREE.Color(1, 1, 1),
-                //wireframe:true,
-                transparent: true,
-                opacity: .5
-
-            });
-
-        }
-
-    };
-
     // for each day in the response
     days.forEach(function (day, i) {
 
@@ -255,18 +151,10 @@ $.ajax({
                     // geometry
                     new THREE.BoxGeometry(d, h, w),
 
-                    // material(s)
-                    materials.set({
+                    materials.standard())
 
-                        day: day,
-                        data: data
-                        //bestDay: data.bestDay,
-                        //jsDate: jsDate
-
-                    }));
-
-            // set BOX position
-            var firstDay = new Date(jsDate.getFullYear(), jsDate.getMonth(), 1),
+                // set BOX position
+                var firstDay = new Date(jsDate.getFullYear(), jsDate.getMonth(), 1),
             yearX = (jsDate.getFullYear() - 2017) * 6 * 12,
             monthX = (jsDate.getMonth() * 6),
             dayX = Math.floor((firstDay.getDay() + jsDate.getDate() - 1) / 7), //Math.floor(jsDate.getDate() / 7),
