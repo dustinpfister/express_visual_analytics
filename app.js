55558ff799a8fa9e8b31778dbf28a7d0cd0e6bcc
dustinpfister <dustin.pfister@gmail.com>
2018-06-29 16:58:46 -0400

    added in bootstrap

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.set('view engine', 'ejs');
 // STATIC PATHS
 app.use('/js', express.static('public/js'));
 app.use('/json', express.static('public/json'));
+app.use('/css', express.static('public/css'));
+
+// theme statics
+app.use('/theme/js', express.static( path.join(__dirname,'themes',app.get('theme'),'js') ));
+app.use('/theme/fonts', express.static( path.join(__dirname,'themes',app.get('theme'),'fonts') ));
+app.use('/theme/css', express.static( path.join(__dirname,'themes',app.get('theme'),'css') ));
+
 
 // MAIN INDEX
 app.get('/', function (req, res) {
