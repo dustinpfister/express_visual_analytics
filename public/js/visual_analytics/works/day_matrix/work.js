b6c409fe07d7532a210d0edb7fae461d7bf97cb5
dustinpfister <dustin.pfister@gmail.com>
2018-07-02 10:43:39 -0400

    placing cubes over each bar

--- public/js/visual_analytics/works/day_matrix/work.js
+++ public/js/visual_analytics/works/day_matrix/work.js
@@ -8,6 +8,15 @@ getData.get(function (data) {
     // using three_shell.js
     shell = new threeShell.StandardScene();
 
+    var highest = 0;
+    days.forEach(function (day, i) {
+
+        if (Number(day.users) > highest) {
+
+            highest = Number(day.users);
+
+        }
+    });
 
     // for each day in the response
     days.forEach(function (day, i) {
@@ -18,24 +27,47 @@ getData.get(function (data) {
 
             // The Box for this day
             var d = 1,
-            h = day.users * .125,
-            w = 1,
-            box = new THREE.Mesh(
+            h = day.users / highest * 10,
+            w = 1;
 
+            var bar = new THREE.Mesh(
                     // geometry
                     new THREE.BoxGeometry(d, h, w),
-
                     materials.standard());
 
-            // box.position.set(i* 2, h/2, i *2);
-			
-			var row = Math.floor(i/28),
-			col = i % 28;
-			
-			box.position.set(row*2,h/2,col*2);
+            var row = Math.floor(i / 28),
+            col = i % 28;
 
-            // and the box to the scene
-            shell.scene.add(box);
+            if (h > 4) {
+
+                console.log(highest);
+
+            }
+
+            bar.position.set(row * 2, h / 2, col * 2);
+
+            var cube = new THREE.Mesh(
+                    // geometry
+                    new THREE.BoxGeometry(1, 1, 1),
+                    (function () {
+
+                        return new THREE.MeshBasicMaterial({
+
+                            //wireframe: true
+                            color: 0x000000,
+                            transparent: true,
+                            opacity: .5
+
+                        })
+
+                    }
+                        ()));
+
+            cube.position.set(row * 2, h / 2 * 2 + .5, col * 2);
+
+            //  add the bar
+            shell.scene.add(bar);
+            shell.scene.add(cube);
 
         }
 
@@ -44,7 +76,8 @@ getData.get(function (data) {
     // start the camera here
     shell.startCamera({
 
-        position: [50,50,50]
+        position: [-4.862419273850179, 15.387518615916813, -4.764027047735164],
+        lookAt: [3.7836326754453897, 8.462837651634782, 3.236078193879614]
 
     });
 
